888b1d9dae9756d1f67a245b786685cec45c3f4b
GameWhaleKingdom <emojiuniverse9@gmail.com>
2026-07-01 21:29:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -48,8 +48,7 @@ app.post('/generate-token', async (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-    const notification_type = req.body.notification_type;
-    console.log('Webhook received:', notification_type);
+    console.log('📥 Webhook received:', req.body);
     res.status(200).json({ status: 'ok' });
 });
 
